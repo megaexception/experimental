b0e091922a8afeab809d55d1b81d546098357ac6
Sergey Khalavchuk <sergey.khalavchuk@gmail.com>
2018-05-16 10:58:41 +0300

    even more advanced hello world

---
+++ node/app.js
@@ -0,0 +1,4 @@
+const notes = require('./notes.js');
+
+console.log(notes.writehello());
+console.log(notes.adder(81, 90));
--- node/notes.js
+++ node/notes.js
@@ -1,5 +1,13 @@
 const fs = require('fs');
 const os = require('os');
 
-// console.log(os.userInfo())
-fs.appendFile("notes.txt", `hello world, ${os.userInfo()['username']}!\n`);
+function wh() {
+    user = os.userInfo();
+    fs.appendFile("notes.txt", `hello world, ${user['username']}!\n`);
+    return "Done";
+}
+
+module.exports.writehello = wh;
+module.exports.adder = (a,b) => {
+  return a+b;
+}
