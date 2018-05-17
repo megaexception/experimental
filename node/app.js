95271c52015d259716f1c4196a6732a794d9db30
Sergey Khalavchuk <sergey.khalavchuk@gmail.com>
2018-05-17 20:28:04 +0300

    added elementary file line numbering (NL) implementation. fixed some ECMA6 issues.

--- node/app.js
+++ node/app.js
@@ -5,11 +5,13 @@ console.log(notes.adder(81, 90));
 console.log(notes.testLDSH());
 notes.log("this is a test");
 
-var myFunction = () => ({age: 23});
-var myFunction = () => {
-    age: 23
-};
+let myFunction = () => ({age: 23});
+// var myFunction = () => {
+//     age: 23
+// };
 console.log(myFunction());
 
 setTimeout(notes.timeout, 5000, "default");
 console.log("Started timeout");
+
+notes.readfile("notes.txt");
--- node/notes.js
+++ node/notes.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 const os = require('os');
+const readline = require('readline');
 const _ = require("lodash");
 
 function wh() {
     console.log(`Running ${arguments.callee.name} function`);
-    user = os.userInfo();
+    let user = os.userInfo();
     fs.appendFile("notes.txt", `hello world, ${user['username']}!\n`);
     return "Done";
 }
@@ -18,8 +19,23 @@ module.exports.testLDSH = function testLDSH() {
     console.log(`Running ${arguments.callee.name} function`);
     console.log(_.isString(true));
     console.log(_.isString("Foobar"));
-    var fArray = _.uniq(['FOo', 1, 2, 'Foo', 1, 2, 34, 5]);
+    let fArray = _.uniq(['FOo', 1, 2, 'Foo', 1, 2, 34, 5]);
     console.log(`Uniquified array: '${fArray}'.`)
 };
-module.exports.log = msg => console.log(`LOG> ${msg}`);
+module.exports.log = msg => console.log(`LOG:${new Date()}> ${msg}`);
 module.exports.timeout = (arg) => console.log(`Timeout for ${arg}`);
+module.exports.enumlines = (list_lines) => {
+    for (let i = 0; i < list_lines.length; i++) {
+        console.log(`${i}. ${list_lines[i]}`)
+    }
+};
+module.exports.readfile = fname => {
+    let list_lines = [];
+    let input = fs.createReadStream(fname);
+    input.on('end', () => {
+        // console.log(list_lines);
+        module.exports.enumlines(list_lines);
+    });
+    const reader = readline.createInterface({input: input});
+    reader.on('line', line => list_lines.push(line));
+};
