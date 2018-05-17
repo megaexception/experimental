42102afe500f8c0b9510b411d3a6811533f92925
Sergey Khalavchuk <sergey.khalavchuk@gmail.com>
2018-05-17 16:05:27 +0300

    fixed slicing, improved unittest

--- python3/experiments.py
+++ python3/experiments.py
@@ -6,6 +6,12 @@ class Foo:
     Class to demonstrate subscript implementation
     """
 
+    def __init__(self, max_size=None):
+        self.max_size = max_size or 64
+
+    def __len__(self):
+        return self.max_size
+
     def __getitem__(self, i) -> str:
         """
         implement indexing for Foo class
@@ -13,8 +19,33 @@ class Foo:
         :return: string with reference to class instance and index
         """
         if isinstance(i, slice):
-            return f"requested items {i.start} to {i.stop}{f' with step {i.step}' if i.step else ''} from {self}"
-        return f"requested item {i} from {self}"
+            start, stop, step = i.start, i.stop, i.step
+            if start is None:
+                start = 0
+            elif abs(start) > self.max_size:
+                raise IndexError
+            elif start < 0:
+                start = self.max_size + start
+            if step is None:
+                step = 1 if start >= 0 else -1
+            if stop is None:
+                if i.start is None or i.start >= 0:
+                    stop = self.max_size - 1
+                else:
+                    stop = 0
+            elif stop < 0:
+                stop = self.max_size + i.stop
+            # print(f"> {i}=> {start}:{stop}:{step}")
+            items = [i for i in range(start, min(stop, self.max_size - 1), step)]
+            return f"requested items {items} of {self.max_size}"
+        elif isinstance(i, int):
+            if abs(i) > self.max_size:
+                raise IndexError
+            if i < 0:
+                return f"requested item {self.max_size+i} of {self.max_size}"
+            return f"requested item {i} of {self.max_size}"
+        else:
+            raise KeyError
 
 
 if __name__ == "__main__":
@@ -22,4 +53,13 @@ if __name__ == "__main__":
     print(x[1])
     print(x[-1])
     print(x[0])
+    print(x[-10])
+    # print(x[-500])
+    # print(x[500])
+    print(x[0:0])
     print(x[0:10])
+    print(x[0::2])
+    print(x[-1::])
+    print(x[-1:1])
+    print(x[-1::-1])
+    print(x[::])
--- python3/test_experiments.py
+++ python3/test_experiments.py
@@ -17,9 +17,16 @@ class TestExperiments(TestCase):
         Matches result with pattern
         :return:
         """
-        for index in [0, 3, 500, -1]:
-            subscript = self.foo[index]
-            self.assertIn(f"requested item {index}", subscript)
+        for index in [0, 3, 63]:
+            self.assertIn(f"requested item {index}", self.foo[index])
+        for index in [-1, -3, -63]:
+            self.assertIn(f"requested item {len(self.foo)+index}", self.foo[index])
+        for index in [-100, 100, 500]:
+            with self.assertRaises(IndexError):
+                self.foo[index]
+        for index in ['', 'foobar']:
+            with self.assertRaises(KeyError):
+                self.foo[index]
 
     def testSubcriptsSlices(self):
         """
@@ -27,6 +34,17 @@ class TestExperiments(TestCase):
         Matches result with pattern
         :return:
         """
-        for start, stop, step in [(0, 0, None), (0, 10, None), (0, 10, 2)]:
+        for start, stop, step in [(0, 0, None), (0, 10, None), (0, 10, 2), (None, None, None)]:
             subscript = self.foo[start:stop:step]
-            self.assertIn(f"requested items {start} to {stop}{f' with step {step}' if step else ''} from", subscript)
+            if start is None:
+                start = 0
+            if step is None:
+                step = 1 if start >= 0 else -1
+            if stop is None:
+                if start >= 0:
+                    stop = len(self.foo) - 1
+                else:
+                    stop = 0
+            self.assertIn(
+                f"requested items {[ i for i in range(start, min(stop, len(self.foo) - 1), step)]}",
+                subscript)
