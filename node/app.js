1cbffcddd2690c38f75f07117834c9a3ea6bdb71
Sergey Khalavchuk <sergey.khalavchuk@gmail.com>
2018-05-17 18:42:44 +0300

    added nodejs experiments with arrow-functions and timers

--- node/app.js
+++ node/app.js
@@ -3,3 +3,13 @@ const notes = require('./notes.js');
 console.log(notes.writehello());
 console.log(notes.adder(81, 90));
 console.log(notes.testLDSH());
+notes.log("this is a test");
+
+var myFunction = () => ({age: 23});
+var myFunction = () => {
+    age: 23
+};
+console.log(myFunction());
+
+setTimeout(notes.timeout, 5000, "default");
+console.log("Started timeout");
--- node/notes.js
+++ node/notes.js
@@ -3,21 +3,23 @@ const os = require('os');
 const _ = require("lodash");
 
 function wh() {
-  console.log(`Running ${arguments.callee.name} function`)
-  user = os.userInfo();
-  fs.appendFile("notes.txt", `hello world, ${user['username']}!\n`);
-  return "Done";
+    console.log(`Running ${arguments.callee.name} function`);
+    user = os.userInfo();
+    fs.appendFile("notes.txt", `hello world, ${user['username']}!\n`);
+    return "Done";
 }
 
 module.exports.writehello = wh;
 module.exports.adder = (a, b) => {
-  console.log(`Running ${arguments.callee.name} function`)
-  return a + b;
-}
+    console.log(`Running ${arguments.callee.name} function`);
+    return a + b;
+};
 module.exports.testLDSH = function testLDSH() {
-  console.log(`Running ${arguments.callee.name} function`)
-  console.log(_.isString(true))
-  console.log(_.isString("Foobar"))
-  var fArray = _.uniq(['FOo',1,2,'Foo',1,2,34,5])
-  console.log(`Uniquified array: '${fArray}'.`)
-}
+    console.log(`Running ${arguments.callee.name} function`);
+    console.log(_.isString(true));
+    console.log(_.isString("Foobar"));
+    var fArray = _.uniq(['FOo', 1, 2, 'Foo', 1, 2, 34, 5]);
+    console.log(`Uniquified array: '${fArray}'.`)
+};
+module.exports.log = msg => console.log(`LOG> ${msg}`);
+module.exports.timeout = (arg) => console.log(`Timeout for ${arg}`);
